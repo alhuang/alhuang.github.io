3c3c78d4eab188ce715d38f30b9b836aa9fe379b
alhuang <andh@umich.edu>
2021-05-19 14:14:01 -0700

    updated goshen page and info

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,15 +2,37 @@ import React from "react";
 import "./HeroSection.css";
 import "../App.css";
 import { Button } from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/bgvideo_1.mp4" autoPlay loop muted />
       <h1>Andrew Huang</h1>
-      <p>Hello</p>
+      <p>Software Engineer and UX Designer based in Seattle</p>
       <div className="hero-btns">
-        <Button
+        <div className="hero-icons">
+          <a className="hero-icon" href="mailto:ahuang91@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} size="3x" className={"icon"} />
+          </a>
+          <a
+            className="hero-icon"
+            href="https://www.linkedin.com/in/ahuang91/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="3x" className={"icon"} />
+          </a>
+          <a
+            className="hero-icon"
+            href="https://github.com/alhuang"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faGithub} size="3x" className={"icon"} />
+          </a>
+        </div>
+        {/* <Button
           className="btns"
           buttonStyle="btn--outline"
           buttonSize="btn--large"
@@ -23,7 +45,7 @@ function HeroSection() {
           buttonSize="btn--large"
         >
           Button 2
-        </Button>
+        </Button> */}
       </div>
     </div>
   );
